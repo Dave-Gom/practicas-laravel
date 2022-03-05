3add36083c86216d48c3a2d1d173b21e22476fef
Dave Gomez <davegomez426@gmail.com>
2022-03-04 21:42:40 -0300

    insertando Factories

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,19 +12,19 @@ class ProductController extends Controller
   public function create(){
     return 'this is the form to create a product';
   }
-  public function store(){
-    //
-  }
+  // public function store(){
+  //   //
+  // }
   public function show($product){
     return "mosatrando product con id {$product}";
   }
-  public function edit(){
+  public function edit($product){
     return "showing the form to edit the product with the id {$product}";
   }
-  public function update(){
+  // public function update(){
 
-  }
-  public function destroy(){
+  // }
+  // public function destroy(){
 
-  }
+  // }
 }
